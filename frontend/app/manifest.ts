b876944c73b275eaf6254f702d98934286c77d83
jiho <jihopark09@naver.com>
2025-03-07 14:30:46 +0900

    fix(fe): change icon directory for manifest file

--- frontend/app/manifest.ts
+++ frontend/app/manifest.ts
@@ -14,12 +14,12 @@ export default function manifest(): MetadataRoute.Manifest {
     scope: '/',
     icons: [
       {
-        src: 'images/icons/icon-192x192.png',
+        src: 'icons/icon-192x192.png',
         sizes: '192x192',
         type: 'image/png',
       },
       {
-        src: 'images/icons/icon-512x512.png',
+        src: 'icons/icon-512x512.png',
         sizes: '512x512',
         type: 'image/png',
       },
