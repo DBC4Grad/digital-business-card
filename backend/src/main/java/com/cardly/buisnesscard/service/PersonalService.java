00a42b131803f88491f439dcc743dd88497c298b
gongjuheon <yh6543210@gmail.com>
2025-04-24 00:57:35 +0900

    feat(be): add getPersonalCardByUsername() method

--- backend/src/main/java/com/cardly/buisnesscard/service/PersonalService.java
+++ backend/src/main/java/com/cardly/buisnesscard/service/PersonalService.java
@@ -6,6 +6,7 @@ public interface PersonalService {
 
     Personal createOrUpdatePersonalCard(Long userId, Personal personal);
     Personal getPersonalCardByUser(Long userId);
+    Personal getPersonalCardByUsername(String username);
     Personal getPersonalCardByQrHash(String qrHash);
     void deletePersonalCard(Long userId);
 }
