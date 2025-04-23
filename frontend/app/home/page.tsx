35e17db7553970c22200d508c0c013bc19b2ee04
jiho <jihopark09@naver.com>
2025-04-23 21:33:51 +0900

    change folder structure

--- frontend/app/home/page.tsx
+++ frontend/app/home/page.tsx
@@ -1,17 +1,15 @@
 'use client';
 
 import { useState, useEffect } from 'react';
-import { Wallet, UserSquare2, Settings } from 'lucide-react';
 import MyCard from '@/components/my-card';
-import CardsList from '@/components/cards-list';
-import Header from '@/components/Header';
 import { useAuth } from '@/app/context/AuthContext';
 import { UserData } from '@/types/type';
-import CreateCard from '@/components/createCard';
-import SettingsPage from '@/components/settings';
+import { usePathname } from 'next/navigation';
+import Link from 'next/link';
 
 export default function BusinessCardApp() {
-  const [activeTab, setActiveTab] = useState<'myCard' | 'cardsList' | 'settings' | 'createCard'>('myCard');
+  const pathname = usePathname(); // Get the current URL path
+  const page = pathname.split('/home/')[1] || '';
   const [userData, setUserData] = useState<UserData>({
     username: '',
     personal: {
@@ -26,7 +24,7 @@ export default function BusinessCardApp() {
   }); // State to hold user data
   const { user } = useAuth();
   // 임시로 true로 해놓음
-  const [hasPersonalCard, setHasPersonalCard] = useState<boolean | null>(true); // State to check if the user has a personal card
+  const [hasPersonalCard, setHasPersonalCard] = useState<boolean | null>(false); // State to check if the user has a personal card
 
   useEffect(() => {
     const fetchUserData = async () => {
@@ -59,49 +57,37 @@ export default function BusinessCardApp() {
   }, [user]);
 
   return (
-    <div className="flex flex-col items-center min-h-screen bg-[#e8f2dd]">
-      {/* Header */}
-      <Header />
-
-      {/* Content */}
-      <div
-        className={`w-full flex-1 flex flex-col items-center rounded overflow-y-auto ${
-          activeTab === 'cardsList' ? 'pb-24' : ''
-        }`}
-      >
-        {activeTab === 'myCard' && (
-          <>
-            {hasPersonalCard === null ? (
-              <p className="text-lg text-[#6a8d5d]">Loading...</p>
-            ) : hasPersonalCard ? (
-              <MyCard userData={userData} />
-            ) : (
-              <div className="flex flex-col items-center justify-center h-full">
-                <p className="text-lg text-[#6a8d5d] mb-4">You do not have a personal card yet.</p>
-                <button
-                  className="px-4 py-2 bg-[#6a8d5d] text-white rounded-md cursor-pointer transition"
-                  onClick={() => setActiveTab('createCard')}
-                >
+    <div
+      className={`w-full flex-1 flex flex-col items-center rounded overflow-y-auto ${
+        page === 'cardsList' ? 'pb-24' : ''
+      }`}
+    >
+      {page === '' && (
+        <>
+          {hasPersonalCard === null ? (
+            <p className="text-lg text-[#6a8d5d]">Loading...</p>
+          ) : hasPersonalCard ? (
+            <MyCard userData={userData} />
+          ) : (
+            <div className="flex flex-col items-center justify-center h-full">
+              <p className="text-lg text-[#6a8d5d] mb-4">You do not have a personal card yet.</p>
+              <Link href="/home/createCard">
+                <button className="px-4 py-2 bg-[#6a8d5d] text-white rounded-md cursor-pointer transition">
                   Create Personal Card
                 </button>
-              </div>
-            )}
-          </>
-        )}
-        {activeTab === 'cardsList' && <CardsList />}
-        {activeTab === 'settings' && <SettingsPage />}
-        {activeTab === 'createCard' && <CreateCard />}
+              </Link>
+            </div>
+          )}
+        </>
+      )}
+    </div>
+  );
 
-        {/* `{user && activeTab === 'myCard' && <MyCard />}
-        {user && activeTab === 'cardsList' && <CardsList />}
-        {user && activeTab === 'settings' && (
-          <div className="flex items-center justify-center h-full">
-            <p className="text-lg text-[#6a8d5d]">Settings Page</p>
-          </div>
-        )}` */}
-
-        {/* 애초에 로그인이 안되면 이 페이지들로 갈 일이 없기는 하지만, 임시로 밑에 화면 만들어놓음 */}
-        {/* {!user && activeTab === 'myCard' && (
+  {
+    /* 애초에 로그인이 안되면 이 페이지들로 갈 일이 없기는 하지만, 임시로 밑에 화면 만들어놓음 */
+  }
+  {
+    /* {!user && activeTab === 'myCard' && (
           <div className="flex items-center justify-center h-full">
             <p className="text-lg text-[#6a8d5d]">Please log in to view your card.</p>
           </div>
@@ -115,46 +101,6 @@ export default function BusinessCardApp() {
           <div className="flex items-center justify-center h-full">
             <p className="text-lg text-[#6a8d5d]">Please log in to access settings.</p>
           </div>
-        )} */}
-      </div>
-
-      {/* Bottom Navigation */}
-      {activeTab === 'cardsList' || activeTab === 'createCard' ? (
-        <div className="fixed bottom-0 w-[447px] h-[130px] bg-[#e8f2dd] p-2 flex justify-around z-20">
-          <div className="fixed bottom-8 w-[400px] max-w-md bg-white rounded-full shadow-xl p-2 flex justify-around z-30">
-            <button
-              className={`p-3 ${activeTab === 'cardsList' ? 'bg-[#d8e8c9] rounded-full' : ''}`}
-              onClick={() => setActiveTab('cardsList')}
-            >
-              <Wallet className="w-6 h-6" />
-            </button>
-            <button className={`p-3`} onClick={() => setActiveTab('myCard')}>
-              <UserSquare2 className="w-6 h-6" />
-            </button>
-            <button className={`p-3`} onClick={() => setActiveTab('settings')}>
-              <Settings className="w-6 h-6" />
-            </button>
-          </div>
-        </div>
-      ) : (
-        <div className="fixed bottom-8 w-[400px] max-w-md bg-white rounded-full shadow-xl p-2 flex justify-around z-30">
-          <button className={`p-3`} onClick={() => setActiveTab('cardsList')}>
-            <Wallet className="w-6 h-6" />
-          </button>
-          <button
-            className={`p-3 ${activeTab === 'myCard' ? 'bg-[#d8e8c9] rounded-full' : ''}`}
-            onClick={() => setActiveTab('myCard')}
-          >
-            <UserSquare2 className="w-6 h-6" />
-          </button>
-          <button
-            className={`p-3 ${activeTab === 'settings' ? 'bg-[#d8e8c9] rounded-full' : ''}`}
-            onClick={() => setActiveTab('settings')}
-          >
-            <Settings className="w-6 h-6" />
-          </button>
-        </div>
-      )}
-    </div>
-  );
+        )} */
+  }
 }
